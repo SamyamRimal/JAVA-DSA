c9c3d19f1263e0039f991919dc7a146540dc9fdc
SamyamRimal <samyamrimal@gmail.com>
2024-11-26 21:43:42 +0545

    Assending/Desending/Largest

--- JAVA-DSA/bubble.java
+++ JAVA-DSA/bubble.java
@@ -7,10 +7,11 @@ public class bubble {
     }
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
+        
         for(int i=0; i<n-1 ; i++){
             for(int j=0; j<n-i-1; j++){
-                if(arr[j]>arr[j+1]){
-                int temp = arr[j];
+                if(arr[j]<arr[j+1]){  // This single symbole of < / > can change from ascending to decending
+                 int temp = arr[j];
                 arr[j] = arr [j+1];
                 arr[j+1] = temp;
                 }
