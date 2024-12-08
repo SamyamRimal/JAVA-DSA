c97db086cf5985bc573a72d6dd9bf8fdd004a86e
SamyamRimal <samyamrimal@gmail.com>
2024-12-08 23:27:37 +0545

    Adding Digits

---
+++ JAVA-DSA/SumOfDigits.java
@@ -0,0 +1,14 @@
+public class SumOfDigits {
+
+    public static int sumDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n % 10 + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        int number = 5936;
+        System.out.println("Sum of digits: " + sumDigits(number));  
+    }
+}
