a109204d6de79a504a733912c2a54b0a0259b4c2
SamyamRimal <samyamrimal@gmail.com>
2024-12-22 22:00:49 +0545

    Learning More

---
+++ JAVA-DSA/ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+
+    public static String reverse(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String input = "hello"; 
+        String result = reverse(input);
+        System.out.println("The reverse of \"" + input + "\" is: \"" + result + "\"");
+    }
+}
