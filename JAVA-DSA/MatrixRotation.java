4bc2569c64b353dbfa96fb62c1acf8aafe33ffc3
SamyamRimal <samyamrimal@gmail.com>
2025-01-01 22:59:02 +0545

    Matrix

--- JAVA-DSA/MatrixRotation.java
+++ JAVA-DSA/MatrixRotation.java
@@ -1,6 +1,29 @@
 import java.util.Scanner;
 
 public class MatrixRotation {
+    // Method to rotate a matrix 90 degrees clockwise
+    public static void rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
