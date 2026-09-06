74236e5f50ba769c1afebe2df03d2bbc31031732
SamyamRimal <samyamrimal@gmail.com>
2026-09-06 21:55:48 +0545

    Learned about Variables and Data Types

---
+++ JAVA/dataType.java
@@ -0,0 +1,15 @@
+public class dataType {
+    public static void main(String[] args) {
+        int a = 10; // Integer data type
+        double b = 20.5; // Double data type
+        char c = 'A'; // Character data type
+        boolean d = true; // Boolean data type
+        String e = "Hello"; // String data type
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("String: " + e);
+    }
+}
---
+++ JAVA/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        String name = "Samyam Rimal";
+        System.out.println("Sum: " + sum);
+        System.out.println("Name: " + name);
+    }
+}
