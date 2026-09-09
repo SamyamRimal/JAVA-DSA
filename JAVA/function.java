4483bb69a8445c542dfed9f0b153984d923d3190
SamyamRimal <samyamrimal@gmail.com>
2026-09-09 23:30:54 +0545

    Basic of Function

---
+++ JAVA/function.java
@@ -0,0 +1,9 @@
+public class function{
+    public static void sum(int a, int b){
+        System.out.print(a+b);
+    }
+    
+    public static void main(String[] args){
+        sum(10,15); //Function call
+    }
+}
