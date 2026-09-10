c158d27c27d630d76be5bcaac38abbc527167a61
SamyamRimal <samyamrimal@gmail.com>
2026-09-10 23:01:23 +0545

    Function bit more advanced

--- JAVA/Function/factorial.java
+++ JAVA/Function/factorial.java
@@ -1,15 +1,25 @@
 public class factorial {
-    
-    public static int factorial(int n){
-        int f=1;
-        for(int i=1; i<=n; i++){
-            f*=i;
+
+    public static int factorial(int n) {
+        int f = 1;
+
+        for (int i = 1; i <= n; i++) {
+            f *= i;
         }
+
         return f;
     }
-    
-    public static void main(String[] args){
-        int result = factorial(5);
+
+    public static int binCoeff(int n, int r) {
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int nmr_fact = factorial(n - r);
+
+        return n_fact / (r_fact * nmr_fact);
+    }
+
+    public static void main(String[] args) {
+        int result = binCoeff(5, 2);
         System.out.print(result);
     }
 }
