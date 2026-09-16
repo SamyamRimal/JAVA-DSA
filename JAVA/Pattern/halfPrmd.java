6b2905cd1ed45fb0186983b05e56b0386cbc1d51
SamyamRimal <samyamrimal@gmail.com>
2026-09-16 23:03:29 +0545

    Inverted Half Pyramid with Numbers

--- JAVA/Pattern/halfPrmd.java
+++ JAVA/Pattern/halfPrmd.java
@@ -1,11 +1,18 @@
 public class halfPrmd {
     public static void main(String[] args){
-        for(int i=1; i<=4; i++){
-            for(int j=1; j<=4-i; j++){
-                System.out.print(" ");
-            }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
+        // for(int i=1; i<=4; i++){
+        //     for(int j=1; j<=4-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=6-i; j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
