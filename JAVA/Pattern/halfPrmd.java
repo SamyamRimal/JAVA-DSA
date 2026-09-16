8df305faed9120937dead67335bab4f89596aa9b
SamyamRimal <samyamrimal@gmail.com>
2026-09-16 23:15:32 +0545

    FLOYD'S Triangle

--- JAVA/Pattern/halfPrmd.java
+++ JAVA/Pattern/halfPrmd.java
@@ -10,9 +10,23 @@ public class halfPrmd {
         //     System.out.println();
         // }
 
+
+
+    //Inverted Half Pyramid with Numbers
+        // for(int i=1; i<=5; i++){
+        //     for(int j=1; j<=6-i; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+    // FLOYD'S Triangle
+    int counter = 1;
         for(int i=1; i<=5; i++){
-            for(int j=1; j<=6-i; j++){
-                System.out.print(j+" ");
+            for(int j=1;j<=i;j++){ 
+                System.out.print(counter+" ");
+                counter++;
             }
             System.out.println();
         }
