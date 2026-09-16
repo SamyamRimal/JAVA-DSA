7d3340ce9b46e3586446f6c1891ed0a7108b4ada
SamyamRimal <samyamrimal@gmail.com>
2026-09-16 22:46:57 +0545

    Patterns Practice

--- JAVA/Pattern/hollowRect.java
+++ JAVA/Pattern/hollowRect.java
@@ -2,13 +2,13 @@ public class hollowRect {
     
     public static void pattern(int row, int col){
         //Outer Loop
-        for (int i=1; i<=row, i++){
+        for (int i=1; i<=row; i++){
             for(int j=1; j<=col; j++){
-                if(i==1 || i == rows || j ==1 || j == col){
-                    System.out.print("*");
+                if(i==1 || i == row || j ==1 || j == col){
+                    System.out.print("* ");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
