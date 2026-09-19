d1458231f98f280d1766cff8435c9062383ef7a1
SamyamRimal <samyamrimal@gmail.com>
2026-09-19 21:35:01 +0545

    Understanding Patterns

--- JAVA/Pattern/halfPrmd.java
+++ JAVA/Pattern/halfPrmd.java
@@ -22,11 +22,26 @@ public class halfPrmd {
 
 
     // FLOYD'S Triangle
+//     int counter = 1;
+//         for(int i=1; i<=5; i++){
+//             for(int j=1;j<=i;j++){ 
+//                 System.out.print(counter+" ");
+//                 counter++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+    // 0-1 Triangle
     int counter = 1;
         for(int i=1; i<=5; i++){
-            for(int j=1;j<=i;j++){ 
-                System.out.print(counter+" ");
-                counter++;
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0) 
+                System.out.print("1 ");
+                else{
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
